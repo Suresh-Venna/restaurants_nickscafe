07843f1713be483592333307cf8bfc5f987d3f2d
Venkata Suresh Venna <venkatasureshvenna@gmail.com>
2022-03-09 18:39:14 -0600

    added staticfiles_dirs

--- nickscafeproject/nickscafeproject/settings.py
+++ nickscafeproject/nickscafeproject/settings.py
@@ -123,8 +123,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 STATICFILES_DIRS = [
-    BASE_DIR / "static/",
-    '/home/ubuntu/static'
+    BASE_DIR / "static/"
 
 ]
 
